2aeacf9e4ef2af8e1d8ea18258121765f74e4e63
owasim1 <omar.wasim@valpo.edu>
2017-07-10 16:54:11 -0700

    added two controllers

---
+++ myApp/myApp/ResultsController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultsController.swift
+//  myApp
+//
+//  Created by Omar Wasim on 7/10/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ResultsController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ myApp/myApp/SearchPageController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchPageController.swift
+//  myApp
+//
+//  Created by Omar Wasim on 7/10/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class SearchPageController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
