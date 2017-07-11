e8c82fb91b1079174979873b35f2723c0f3e5631
owasim1 <omar.wasim@valpo.edu>
2017-07-11 10:29:41 -0700

    Created first segue

---
+++ myApp/myApp/ResultsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ResultsTableViewCell.swift
+//  myApp
+//
+//  Created by Omar Wasim on 7/11/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ResultsTableViewCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
